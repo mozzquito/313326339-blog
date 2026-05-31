a49159bc4ebfc35bcdf481d664e8fd0e2c0bc382
hx-phongcheat <phongcheat@harmonyx.co>
2026-05-31 13:15:46 +0700

    fix: remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://313326339.xyz',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
 });
